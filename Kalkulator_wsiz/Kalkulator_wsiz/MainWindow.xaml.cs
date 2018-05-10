0a33d484e60f1e62b8fbd715f9364eeee4903904
kamil10074 <w57132@poczta.wsiz.rzeszow.pl>
2018-05-10 20:42:01 +0200

    Dodanie funkcji przycisków numerycznych

--- Kalkulator_wsiz/Kalkulator_wsiz/MainWindow.xaml.cs
+++ Kalkulator_wsiz/Kalkulator_wsiz/MainWindow.xaml.cs
@@ -18,21 +18,44 @@ namespace Kalkulator_wsiz
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    enum Operation
+    {
+        none = 0, // brak operacji
+        addition, // dodawanie
+        subtraction, // odejmowanie
+        multiplication, // mnożenie
+        division, // dzielenie
+        result, // wynik
+        mod, //modulo
+        exp //funkcja eksponencjalna
+
+
+    }
     public partial class MainWindow : Window
     {
+        private Operation m_eLastOperationSelected = Operation.none;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void BackspaceButton_Click(object sender, RoutedEventArgs e)
+        private void NumberButton_Click(object oSender, RoutedEventArgs eRoutedEventArgs)
         {
-
+            if (Operation.result == m_eLastOperationSelected)
+            {
+                txtDisplay.Text = string.Empty;
+                m_eLastOperationSelected = Operation.none;
+            }
+            Button oButton = (Button)oSender;
+            txtDisplay.Text += oButton.Content;
         }
 
-        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        private void MenuItem_Standard_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Width = 240;
+            txtDisplay.Width = 210;
         }
+
     }
 }
