260cab6f2e91f4bfc29935c0a7d89204aa17b659
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-05-23 16:38:39 +0200

    Dodanie wyglądu funkcji kwadratowej

--- Kalkulator_wsiz/Kalkulator_wsiz/Funkcja.xaml.cs
+++ Kalkulator_wsiz/Kalkulator_wsiz/Funkcja.xaml.cs
@@ -1,28 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Kalkulator_wsiz
 {
     /// <summary>
-    /// Interaction logic for Funkcja.xaml
+    /// Interaction logic for Page1.xaml
     /// </summary>
     public partial class Funkcja : Page
     {
         public Funkcja()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+
+            (sender as TextBox).Clear();
+
         }
     }
 }
