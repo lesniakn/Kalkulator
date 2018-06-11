890232c0c0923108f8e972644e1019f48e439e2b
Tczernik <tomasz.czernik1997@gmail.com>
2018-06-11 17:12:55 +0200

    Optymalizacja kodu

--- Kalkulator_wsiz/Kalkulator_wsiz/Funkcja.xaml.cs
+++ Kalkulator_wsiz/Kalkulator_wsiz/Funkcja.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Kalkulator_wsiz
 {
@@ -351,6 +352,46 @@ namespace Kalkulator_wsiz
 
 
         }
+        private void tb_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
 
+                case Key.NumPad0:
+                case Key.NumPad1:
+                case Key.NumPad2:
+                case Key.NumPad3:
+                case Key.NumPad4:
+                case Key.NumPad5:
+                case Key.NumPad6:
+                case Key.NumPad7:
+                case Key.NumPad8:
+                case Key.NumPad9:
+                case Key.D0:
+                case Key.D1:
+                case Key.D2:
+                case Key.D3:
+                case Key.D4:
+                case Key.D5:
+                case Key.D6:
+                case Key.D7:
+                case Key.D8:
+                case Key.D9:
+                case Key.Subtract:
+                case Key.OemMinus:
+
+                    break;
+
+
+                default:
+
+                    e.Handled = true;
+
+
+                    break;
+            }
+        }
+
+       
     }
 }
